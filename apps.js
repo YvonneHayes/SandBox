55413b02e656d9b4ac41183fde8c2123c5018549
Yvonne Hayes <queyvonne@gmail.com>
2016-04-21 13:53:52 -0700

    same HOF now printing to page instead of console logging

--- apps.js
+++ apps.js
@@ -1,13 +1,19 @@
 function useNext(next) {
   console.log('Here\'s a few things I like doing in my free time:');
+  document.getElementById('interests').innerHTML = 'Here\'s a few things I like doing in my free time:';
 
   var likes = ['coding', 'gaming', 'reading'];
   likes.forEach(function(el) { next(el);});
-  console.log('');
 }
 
-function hobbies(s) { console.log('  I enjoy ' + s ); }
-// function goo(s) { console.log('  The string "' + s + '" has ' + s.length + ' characters in it'); }
+function hobbies(s) {
+  console.log('  I enjoy ' + s );
+  var node = document.createElement('li');
+  var textnode = document.createTextNode(s);
+  node.appendChild(textnode);
+  document.getElementById('hobbies').appendChild(node);
+  // document.getElementById('hobbies').innerHTML = 'I enjoy ' + s;
+  // console.log(document.getElementById('hobbies'));
+}
 
 useNext(hobbies);
-// useNext(goo);
