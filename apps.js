13487102ed0d18e6efc86fcc443cf001b596d108
Yvonne Hayes <queyvonne@gmail.com>
2016-04-24 12:39:20 -0700

    basic handlebars template working

--- apps.js
+++ apps.js
@@ -1,1 +1,11 @@
+var Data = {
+  name: 'Yvonne',
+  like: 'gaming',
+  city: 'portland'
+};
 
+var templateScript = $('#tryThis').html();
+
+var template = Handlebars.compile(templateScript);
+
+$(document.body).append(template(Data));
