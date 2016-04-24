6aa0268754d14cb18d90c5df00c9c0f20010d750
Yvonne Hayes <queyvonne@gmail.com>
2016-04-24 12:20:40 -0700

    added JQuery library and tried it out on one DOM element

--- apps.js
+++ apps.js
@@ -1,1 +1,31 @@
+var p = document.querySelector('p');
 
+function setRouteMappings() {
+  page.base('/');
+
+  page('', index);
+  page('about', about);
+  page('contact', contact);
+  page('game',game);
+  page('contact/:contactName', contact);
+  page('*', notfound); // catch-all
+
+  page();
+}
+
+function index() { p.textContent = 'viewing index (home)'; }
+function about() { p.textContent = 'viewing about'; }
+function contact(ctx) {
+  p.textContent = 'viewing contact ' + (ctx.params.contactName || '');
+}
+function notfound() {
+  p.innerHTML ='<h3>OMG, page not found!</h3><img src="lost.jpg"></img>';
+}
+
+// function game() { p.textContent = 'play the game!';}
+
+function game (){
+  $('p').text('PLAY IT!');
+}
+
+setRouteMappings();
