9b50d14f3d010491ca43c703ad76558b589a275d
Yvonne Hayes <queyvonne@gmail.com>
2016-05-02 11:43:29 -0700

    getting input and displaying it back

---
+++ apps.js
@@ -0,0 +1,17 @@
+
+
+
+$(document).ready(function(){
+  $('button').click(function(){
+    var userName = $('input:text').val();
+    useName(userName);
+    localStorage.setItem('currentUser', userName);
+    return userName;
+  });
+});
+
+
+function useName (currentUser){
+  console.log(currentUser);
+  $('#output').text('Hey ' + currentUser + ', what a great day for biking!');
+};
