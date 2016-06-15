abb6a25d22e9137f1b558ecfbc01e3876c401e8a
Yvonne Hayes <queyvonne@gmail.com>
2016-06-15 14:19:56 -0700

    added mob, player attack and stamina logic

--- app.js
+++ app.js
@@ -1,16 +1,62 @@
 var player = {
   name: 'Link',
-  hp: 100
+  hp: 20,
+  stamina: 20
+};
+
+var mob = {
+  name: 'FancySnake',
+  hp: 50
 };
 
 function getsHit(){
   var newHp = player.hp - 5;
-  console.log(newHp);
+  console.log('HP: ' + newHp);
   player.hp = newHp;
+
+  if (player.hp <= 0) {
+    console.log('Game Over');
+  }
 }
 
 getsHit();
+getsHit();
+getsHit();
+getsHit();
 
-getsHit();
-getsHit();
-getsHit();
+function attacksMelee(){
+
+  if(player.stamina < 5) {
+
+    console.log('You are out of stamina');
+    //Nothing happens
+
+  } else {
+
+    var newMobHp = mob.hp - 5;
+    console.log('Mob HP: ' + newMobHp);
+    mob.hp = newMobHp;
+    var newStamina = player.stamina -5;
+    console.log('Stamina: ' + newStamina);
+    player.stamina = newStamina;
+
+    if(mob.hp <= 0) {
+      console.log('You win, moving on!');
+    }
+  }
+}
+
+function attackAndStaminaRegen(){
+
+  attacksMelee();
+  var newStamina = player.stamina + 3;
+  player.stamina = newStamina;
+}
+
+attackAndStaminaRegen();
+attackAndStaminaRegen();
+attackAndStaminaRegen();
+attackAndStaminaRegen();
+attackAndStaminaRegen();
+attackAndStaminaRegen();
+attackAndStaminaRegen();
